d3c0e588f5f8873d74b3e877d3bf7035277c7a4f
Philipp Sepin <151570662+p0017@users.noreply.github.com>
2025-12-01 19:45:38 +0100

    Finished training script, train pipeline works

--- config.py
+++ config.py
@@ -1,14 +1,27 @@
-
 class GPTConfig:
     """Configuration for the GPT model."""
-    vocab_size: int = 65                                    # Make sure this matches the dataset
-    context_length: int = 128                               # Length of the context window
-    n_layer: int = 4                                        # Number of Transformer blocks
-    n_attention_heads: int = 8                              # Number of attention heads
-    embedding_dimension: int = 256                          # Embedding dimension, must be divisible by n_attention_heads
-    dropout_rate: float = 0.1                               # Chose a slightly lower dropout for the small model and dataset
-    learning_rate: float = 3e-4                             # Learning rate
-    batch_size: int = 64                                    # Batch size
-    betas: tuple = (0.9, 0.95)                              # AdamW optimizer betas
-    train_from_scratch: bool = True                         # Whether to train the model from scratch
-    name: str = "tinyshakespeare_V0.0"                      # Name of the experiment for logging and checkpoints
+
+    vocabulary_size: int = 65  # Make sure this matches the dataset
+    context_length: int = 128  # Length of the context window
+    n_layers: int = 4  # Number of Transformer blocks
+    n_attention_heads: int = 8  # Number of attention heads
+    embedding_dimension: int = (
+        256  # Embedding dimension, must be divisible by n_attention_heads
+    )
+    dropout: float = (
+        0.1  # Chose a slightly lower dropout for the small model and dataset
+    )
+    bias: bool = False  # Whether to use bias
+    learning_rate: float = 3e-4  # Learning rate
+    min_learning_rate: float = 1e-5  # Minimum learning rate for cosine decay
+    batch_size: int = 64  # Batch size
+    betas: tuple = (0.9, 0.95)  # AdamW optimizer betas
+    weight_decay: float = 0.1  # Weight decay for AdamW optimizer
+    max_iters: int = 50  # Total number of training iterations
+    warmup_iters: int = 5  # Number of warmup iterations
+    iters_per_eval: int = 5  # Number of iterations for evaluation
+    eval_interval: int = 20  # Interval for evaluation and checkpointing
+    train_from_scratch: bool = True  # Whether to train the model from scratch
+    name: str = (
+        "tinyshakespeare_V0"  # Name of the experiment for logging and checkpoints
+    )
--- train.py
+++ train.py
@@ -1,56 +1,183 @@
 import os
 import numpy as np
 import torch
+import time
+import math
 
 from config import GPTConfig
 from model import GPT
 
 config = GPTConfig()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
+dtype = (
+    "bfloat16"
+    if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+    else "float16"
+)
+
 
 def get_batch(split: str):
-    if split == 'train':
+    if split == "train":
         path = "./data/tinyshakespeare_train.bin"
-    elif split == 'val':
+    elif split == "val":
         path = "./data/tinyshakespeare_val.bin"
-    elif split == 'test':
+    elif split == "test":
         path = "./data/tinyshakespeare_test.bin"
     else:
         raise ValueError("Invalid split name. Use 'train', 'val' or 'test'.")
-    
+
     # Loading the entire dataset into RAM, since its only about 2MB
-    data = np.fromfile(path, dtype=np.uint16)
+    data = np.fromfile(path, dtype=np.uint16).astype(np.int16)
     # Randomly select starting indices for the batch
-    starting_index = torch.randint(len(data) - config.context_length, size=(config.batch_size,))
+    starting_index = torch.randint(
+        len(data) - config.context_length, size=(config.batch_size,)
+    )
     # For each starting index, extract input sequences of length context_length
     # And the corresponding target sequences shifted by one position
-    input = torch.stack([torch.from_numpy(data[i:i+config.context_length]) for i in starting_index])
-    target = torch.stack([torch.from_numpy(data[i+1:i+config.context_length+1]) for i in starting_index])
+    input = torch.stack(
+        [
+            torch.tensor(
+                data[i.item() : i.item() + config.context_length], dtype=torch.long
+            )
+            for i in starting_index
+        ]
+    )
+    target = torch.stack(
+        [
+            torch.tensor(
+                data[i.item() + 1 : i.item() + config.context_length + 1],
+                dtype=torch.long,
+            )
+            for i in starting_index
+        ]
+    )
 
-    if device == 'cuda':
+    if device == "cuda":
         # Pin memory for faster transfer from CPU to GPU
-        input, target = input.pin_memory().to('cuda', non_blocking=True), target.pin_memory().to('cuda', non_blocking=True)
+        input, target = input.pin_memory().to(
+            "cuda", non_blocking=True
+        ), target.pin_memory().to("cuda", non_blocking=True)
     else:
         input, target = input.to(device), target.to(device)
     return input, target
 
+
 if __name__ == "__main__":
     current_iteration = 0
-    best_val_loss = float('inf')
+    best_val_loss = float("inf")
+    best_checkpoint_path = os.path.join("checkpoints", f"{config.name}_best.pt")
 
+    # Either we train from scratch
     if config.train_from_scratch:
         print("Training model from scratch...")
         model = GPT(config)
-        optimizer = model.configure_optimizers(config.weight_decay, config.learning_rate, config.betas)
+        optimizer = model.configure_optimizers(
+            config.weight_decay, config.learning_rate, config.betas, device_type=device
+        )
 
+    # Or we resume from the best previous checkpoint
     else:
-        print("Resuming training model from ./checkpoints/...")
-        checkpoint_path = "./checkpoints/latest.pt"
-        checkpoint = torch.load(checkpoint_path, map_location=device)
-        config = checkpoint['config']
-        current_iteration = checkpoint['iteration']
-        best_val_loss = checkpoint['best_val_loss']
+        print(f"Resuming training model from {best_checkpoint_path}...")
+        if not os.path.exists(best_checkpoint_path):
+            raise FileNotFoundError(f"No checkpoint found at {best_checkpoint_path}")
+
+        checkpoint = torch.load(best_checkpoint_path, map_location=device)
+        config = checkpoint["config"]
+
+        current_iteration = checkpoint["iteration"]
+        best_val_loss = checkpoint["best_val_loss"]
 
         model = GPT(config)
-        model.load_state_dict(checkpoint['model'])
-        optimizer = model.configure_optimizers(config.weight_decay, config.learning_rate, config.betas)
+        model.load_state_dict(checkpoint["model"])
+        optimizer = model.configure_optimizers(
+            config.weight_decay, config.learning_rate, config.betas, device_type=device
+        )
+        # Free up memory
+        checkpoint = None
+
+    model.to(device=device)
+
+    @torch.no_grad()
+    def get_loss():
+        out = {}
+        model.eval()
+        for split in ["train", "val"]:
+            # Evaluating on iters_per_eval batches
+            losses = torch.zeros(config.iters_per_eval)
+            for k in range(config.iters_per_eval):
+                X, Y = get_batch(split)
+                _, loss = model(X, Y)
+                losses[k] = loss.item()
+            # Average loss over the evaluated batches
+            out[split] = losses.mean()
+        model.train()
+        return out
+
+    def get_learning_rate(iteration: int) -> float:
+        # First some warmup
+        if iteration < config.warmup_iters:
+            return config.learning_rate * (iteration + 1) / (config.warmup_iters + 1)
+
+        # Then cosine decay down to min_learning_rate
+        else:
+            decay_ratio = (iteration - config.warmup_iters) / (
+                config.max_iters - config.warmup_iters
+            )
+            coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+            return config.min_learning_rate + coeff * (
+                config.learning_rate - config.min_learning_rate
+            )
+
+    print("Starting training loop...")
+    X, Y = get_batch("train")
+    t0 = time.time()
+
+    while current_iteration <= config.max_iters:
+
+        # Setting the current learning rate
+        lr = get_learning_rate(current_iteration)
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
+
+        # Evaluation and checkpointing every eval_interval iterations
+        if current_iteration % config.eval_interval == 0 and current_iteration > 0:
+            # Estimate time remaining
+            t1 = time.time()
+            dt = t1 - t0
+            t0 = t1
+            iters_left = config.max_iters - current_iteration
+            sec_per_iter = dt / config.eval_interval if current_iteration > 0 else 0
+            eta_sec = iters_left * sec_per_iter
+            eta_str = time.strftime("%H:%M:%S", time.gmtime(eta_sec))
+
+            losses = get_loss()
+            print(
+                f"step {current_iteration}/{config.max_iters} | "
+                f"train loss {losses['train']:.4f} | "
+                f"val loss {losses['val']:.4f} | "
+                f"lr {lr:.2e} | "
+                f"ETA {eta_str}"
+            )
+
+            # Checkpointing the new best model
+            if losses["val"] < best_val_loss:
+                best_val_loss = losses["val"]
+                checkpoint = {
+                    "model": model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "config": config,
+                    "iteration": current_iteration,
+                    "best_val_loss": best_val_loss,
+                }
+
+                print(f"Saving best checkpoint to {best_checkpoint_path}")
+                torch.save(checkpoint, best_checkpoint_path)
+
+        _, loss = model(X, Y)
+        # Preloading the next batch
+        X, Y = get_batch("train")
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+        current_iteration += 1
